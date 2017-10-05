bfffe2864eed015bb15f906db44edda7b87531ba
Elmir Kouliev <elmir@kouliev.com>
2017-10-05 15:16:44 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+global.jQuery = require('jquery');
+import './valiant/valiant360.css';
+
+/**
+ * Stateless Valiant 360 made for react
+ */
+class ReactValiant360 extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        
+    }
+
+    componentDidMount() {
+        this.setUpVRPlayer();
+    }
+
+    componentDidUpdate(prevProps) {
+        const { video } = this.props;
+
+
+    }
+
+    /**
+     * Sets up the VR player using async dependencies
+     */
+    setUpVRPlayer() {
+        const { autoplay, muted } = this.props;
+        global.THREE = require('three');
+        require('script-loader!./valiant/jquery.valiant360.min.js');
+
+        $('#valiant-video-element').Valiant360({
+            hideControls: false,    // hide player controls
+            keyboardControls: true, // use keyboard controls (move by arrows),
+            clickAndDrag: true,
+            autoplay,
+            muted
+        });
+
+    };
+
+    render() {
+        const {
+            width,
+            height,
+            autoplay,
+            hideControls,
+            clickToPlay
+        } = this.props;
+
+        return (
+            <div 
+                className='valiant-vr-wrap' 
+                style={{
+                        height: this.props.height, 
+                        width: this.props.width
+                    }}
+            >
+                <div
+                    className="valiant-vr"
+                    style={{
+                        height: this.props.height, 
+                        width: this.props.width
+                    }}
+                    data-video-src={this.props.video}
+                    id="valiant-video-element">
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactValiant360.defaultProps = {
+    autoplay: false,
+    muted: false,
+    clickToPlay: true,
+    video: '',
+    height: '250px',
+    width: '400px'
+}
+
+ReactValiant360.propTypes = {
+    video: PropTypes.string,
+    thumbnail: PropTypes.string,
+    width: PropTypes.string,
+    type: PropTypes.string,
+    autoplay: PropTypes.bool,
+    clickToPlay: PropTypes.bool,
+    vr: PropTypes.bool,
+    muted: PropTypes.bool,
+    hideControls: PropTypes.bool,
+    highRes: PropTypes.bool,
+    status: PropTypes.number
+}
+
+export default ReactValiant360;
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const package = require('./package.json');
+const webpack = require('webpack');
+const jsPath = `[name].js`;
+const fileLoaderName = '[name].[ext]';
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: jsPath,
+        chunkFilename: jsPath,
+        libraryTarget: 'commonjs2' 
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+        }),
+    ],
+    module: {
+        rules: [
+            // Babel
+            {
+                test: /\.css$/,
+                use: [{ 
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }]
+            },
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /(node_modules|bower_components|build)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: { 
+                    loader: `file-loader?name=${fileLoaderName}`
+                }
+            },
+            {
+                test: /\.exec\.js$/,
+                use: [ 'script-loader' ]
+            }
+        ]
+    },
+    externals: {
+        'react': 'commonjs react'
+    }
+};
